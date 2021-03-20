f99a8c261994d599b9c29e9727f0333e2132f925
ArthurThomson <artsthomson@gmail.com>
2021-03-20 14:20:15 +0200

    Blazor update video 5 split razor components

---
+++ Blazor/BlazorApplication/BlazorServerProject/Pages/Counterbase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerProject.Pages
+{
+    public class Counterbase : ComponentBase
+    {
+        protected int currentCount = 0;
+        protected string fontfamily = "Verdana";
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
