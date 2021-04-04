a24f9c9af8a7f096819e31441c2bf2d2d671fe9e
ArthurThomson <artsthomson@gmail.com>
2021-04-04 14:10:45 +0300

    Employee and Department Repositary creation

---
+++ Blazor Tutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -0,0 +1,69 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetEmployee(int employeeId)
+        {
+            return await appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeID == employeeId);
+        }
+
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await appDbContext.Employees.AddAsync(employee);
+            await appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee employee)
+        {
+            var result = await appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeID == employee.EmployeeID);
+
+            if (result != null)
+            {
+                result.FirstName = employee.FirstName;
+                result.LastName = employee.LastName;
+                result.Email = employee.Email;
+                result.DateOfBirth = employee.DateOfBirth;
+                result.Gender = employee.Gender;
+                result.DepartmentId = employee.DepartmentId;
+                result.PhotoPath = employee.PhotoPath;
+
+                await appDbContext.SaveChangesAsync();
+
+                return result;
+            }
+
+            return null;
+        }
+
+        public async void DeleteEmployee(int employeeID)
+        {
+            var result = await appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeID == employeeID);
+            if (result != null)
+            {
+                appDbContext.Employees.Remove(result);
+                await appDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
