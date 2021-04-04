07929dc8fe959e1f9d5398e7ccb0a8438dd14b6b
ArthurThomson <artsthomson@gmail.com>
2021-04-04 21:39:18 +0300

    Blazor Component parameters

---
+++ Blazor Tutorial/EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DisplayEmployeeBase : ComponentBase
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+
+        [Parameter]
+        public bool ShowFooter { get; set; }
+    }
+}
