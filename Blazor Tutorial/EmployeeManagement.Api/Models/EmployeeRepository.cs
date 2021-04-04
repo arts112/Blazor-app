5ca99eac7368ee96bd3194b4510b3c95b8bbbdfc
ArthurThomson <artsthomson@gmail.com>
2021-04-04 19:34:02 +0300

    Display data from two or more related tables in Blazor

--- Blazor Tutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
+++ Blazor Tutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -23,6 +23,7 @@ namespace EmployeeManagement.Api.Models
         public async Task<Employee> GetEmployee(int employeeId)
         {
             return await appDbContext.Employees
+                .Include(e => e.Department)
                 .FirstOrDefaultAsync(e => e.EmployeeID == employeeId);
 
         }public async Task<Employee> GetEmployeeByEmail(string email)
