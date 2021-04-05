f331b655993b88ee36ef23105d320996c22f0941
ArthurThomson <artsthomson@gmail.com>
2021-04-05 19:17:54 +0300

    Blazor form validation

--- Blazor Tutorial/EmployeeManagement.Models/Employee.cs
+++ Blazor Tutorial/EmployeeManagement.Models/Employee.cs
@@ -10,11 +10,12 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeID { get; set; }
-        
+        [Required]
+        [MinLength(2)]
         public string FirstName { get; set; }
-       
+        [Required]
         public string LastName { get; set; }
-        
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
